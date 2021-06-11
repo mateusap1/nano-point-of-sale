24b9f3f3183be1396bb85a0e21dc83d02a6ef255
Mateus Santos <mateusap1@gmail.com>
2021-06-11 16:10:45 -0300

    Fixed balance bug

--- src/background/manageDB.ts
+++ src/background/manageDB.ts
@@ -485,12 +485,6 @@ export async function getInfo(
         details: details.length > 0 ? details : null,
       });
 
-      if (type === 1) {
-        balanceTotal += amount;
-      } else {
-        balanceTotal -= amount;
-      }
-
       if (
         new Date(date * 1000).setHours(0, 0, 0, 0) ===
           new Date().setHours(0, 0, 0, 0) &&
@@ -498,6 +492,12 @@ export async function getInfo(
       ) {
         balanceToday += amount;
       }
+    }
+
+    if (type === 1) {
+      balanceTotal += amount;
+    } else {
+      balanceTotal -= amount;
     }
   }
 
